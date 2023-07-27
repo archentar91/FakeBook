37f12d43671f5c2e5e0db801aaeda83098313187
archentar91 <archentar@hotmail.com>
2023-07-27 23:13:13 +0200

    Registration added

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SingUpForm
 
 # Create your views here.
 
@@ -25,3 +26,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You are now logged out')
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SingUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login the user
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('home')
+    else:
+        form = SingUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
