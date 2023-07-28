03573ccce73ca9b988c6394a2d3461a6f92442be
archentar91 <archentar@hotmail.com>
2023-07-28 18:21:21 +0200

    Adding, Editing, Updating records added

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
 class Record(models.Model):
@@ -6,13 +7,14 @@ class Record(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    adress = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
+    country = CountryField()
     city = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=30)
-    phone = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+
     
     def __str__(self):
         return(f"{self.first_name} {self.last_name}")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SingUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -32,7 +32,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-        form = SingUpForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             # Authenticate and login the user
@@ -43,7 +43,7 @@ def register_user(request):
             messages.success(request, 'You are now logged in')
             return redirect('home')
     else:
-        form = SingUpForm()
+        form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
 
@@ -64,4 +64,31 @@ def delete_record(request, pk):
         return redirect('home')
     else:
         messages.error(request, 'You are not logged in')
+        return redirect('home')
+    
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added.")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In.")
+		return redirect('home')
+
+def edit_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Updated.")
+            return redirect('home')
+        return render(request, 'edit_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In.")
         return redirect('home')
