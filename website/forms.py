9d288df546aa91f9247db240bf4fcfadd490016a
archentar91 <archentar@hotmail.com>
2023-08-02 08:29:38 +0200

    update ver alpha 0.03

--- website/forms.py
+++ website/forms.py
@@ -19,13 +19,13 @@ class LoginForm(forms.Form):
         
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-        self.fields['username'].label = ''
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Username Required.</small></span>'
+        self.fields['username'].label = 'User name'
+        self.fields['username'].help_text = ''
         
         self.fields['password'].widget.attrs['class'] = 'form-control'
         self.fields['password'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password'].label = ''
-        self.fields['password'].help_text = '<span class="form-text text-muted"><small>Password Required.</small></span>'
+        self.fields['password'].label = 'Password'
+        self.fields['password'].help_text = ''
         
 #Sign Up Form
 class SignUpForm(UserCreationForm):
@@ -42,33 +42,33 @@ class SignUpForm(UserCreationForm):
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-        self.fields['username'].label = ''
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Username Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+        self.fields['username'].label = 'User name'
+        self.fields['username'].help_text = ''
         
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['placeholder'] = 'Email'
-        self.fields['email'].label = ''
-        self.fields['email'].help_text = '<span class="form-text text-muted"><small>Email Adress</small></span>'
-        
+        self.fields['email'].label = 'Email address'
+        self.fields['email'].help_text = ''
+
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
         self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
-        self.fields['first_name'].label = ''
-        self.fields['first_name'].help_text = '<span class="form-text text-muted"><small>First Name Required</small></span>'
+        self.fields['first_name'].label = 'First name'
+        self.fields['first_name'].help_text = ''
         
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
         self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
-        self.fields['last_name'].label = ''
-        self.fields['last_name'].help_text = '<span class="form-text text-muted"><small>Last Name</small></span>'
+        self.fields['last_name'].label = 'Last name'
+        self.fields['last_name'].help_text = ''
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password1'].label = ''
+        self.fields['password1'].label = 'Password'
         self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        self.fields['password2'].label = ''
-        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+        self.fields['password2'].label = 'Password confirmation'
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
 #Add Record Form
 class AddRecordForm(forms.ModelForm):
@@ -90,48 +90,48 @@ class AddRecordForm(forms.ModelForm):
 
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
         self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
-        self.fields['first_name'].label = ''
-        self.fields['first_name'].help_text = '<span class="form-text text-muted"><small>First Name</small></span>'
+        self.fields['first_name'].label = 'First name'
+        self.fields['first_name'].help_text = ''
         
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
         self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
-        self.fields['last_name'].label = ''
-        self.fields['last_name'].help_text = '<span class="form-text text-muted"><small>Last Name</small></span>'
+        self.fields['last_name'].label = 'Last name'
+        self.fields['last_name'].help_text = ''
         
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['placeholder'] = 'Email'
-        self.fields['email'].label = ''
-        self.fields['email'].help_text = '<span class="form-text text-muted"><small>Email Adress</small></span>'
+        self.fields['email'].label = 'Email address'
+        self.fields['email'].help_text = ''
         
         self.fields['phone'].widget.attrs['class'] = 'form-control'
         self.fields['phone'].widget.attrs['placeholder'] = 'Phone'
-        self.fields['phone'].label = ''
-        self.fields['phone'].help_text = '<span class="form-text text-muted"><small>Phone Number</small></span>'
+        self.fields['phone'].label = 'Phone number'
+        self.fields['phone'].help_text = ''
         
         self.fields['country'].widget.attrs['class'] = 'form-control'
         self.fields['country'].widget.attrs['placeholder'] = 'Country'
-        self.fields['country'].label = ''
-        self.fields['country'].help_text = '<span class="form-text text-muted"><small>Country</small></span>'
+        self.fields['country'].label = 'Country'
+        self.fields['country'].help_text = ''
         
         self.fields['city'].widget.attrs['class'] = 'form-control'
         self.fields['city'].widget.attrs['placeholder'] = 'City'
-        self.fields['city'].label = ''
-        self.fields['city'].help_text = '<span class="form-text text-muted"><small>City</small></span>'
+        self.fields['city'].label = 'City'
+        self.fields['city'].help_text = ''
         
         self.fields['address'].widget.attrs['class'] = 'form-control'
         self.fields['address'].widget.attrs['placeholder'] = 'Address' 
-        self.fields['address'].label = ''
-        self.fields['address'].help_text = '<span class="form-text text-muted"><small>Address</small></span>'
+        self.fields['address'].label = 'Address' 
+        self.fields['address'].help_text = ''
         
         self.fields['state'].widget.attrs['class'] = 'form-control'
         self.fields['state'].widget.attrs['placeholder'] = 'State'
-        self.fields['state'].label = ''
-        self.fields['state'].help_text = '<span class="form-text text-muted"><small>State</small></span>'
+        self.fields['state'].label = 'State'
+        self.fields['state'].help_text = ''
         
         self.fields['zipcode'].widget.attrs['class'] = 'form-control'
         self.fields['zipcode'].widget.attrs['placeholder'] = 'Zipcode'
-        self.fields['zipcode'].label = ''
-        self.fields['zipcode'].help_text = '<span class="form-text text-muted"><small>Zipcode</small></span>'
+        self.fields['zipcode'].label = 'Zipcode'
+        self.fields['zipcode'].help_text = ''
         
 #Custom UpdateProfileForm
 class UpdateProfileForm(forms.ModelForm):
@@ -144,23 +144,23 @@ class UpdateProfileForm(forms.ModelForm):
         
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-        self.fields['username'].label = ''
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Enter your first name.</small></span>'
+        self.fields['username'].label = 'User name'
+        self.fields['username'].help_text = ''
 
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
         self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
-        self.fields['first_name'].label = ''
-        self.fields['first_name'].help_text = '<span class="form-text text-muted"><small>Enter your first name.</small></span>'
+        self.fields['first_name'].label = 'First name'
+        self.fields['first_name'].help_text = ''
 
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
         self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
-        self.fields['last_name'].label = ''
-        self.fields['last_name'].help_text = '<span class="form-text text-muted"><small>Enter your last name.</small></span>'
+        self.fields['last_name'].label = 'Last name'
+        self.fields['last_name'].help_text = ''
 
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['placeholder'] = 'Email'
-        self.fields['email'].label = ''
-        self.fields['email'].help_text = '<span class="form-text text-muted"><small>Enter your email address.</small></span>'
+        self.fields['email'].label = 'Email address'
+        self.fields['email'].help_text = ''
 
 class ChangePasswordForm(PasswordChangeForm):
     old_password = forms.CharField(label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Old Password'}))
@@ -172,17 +172,17 @@ class ChangePasswordForm(PasswordChangeForm):
         
         self.fields['old_password'].widget.attrs['class'] = 'form-control'
         self.fields['old_password'].widget.attrs['placeholder'] = 'Old Password'
-        self.fields['old_password'].label = ''
-        self.fields['old_password'].help_text = '<span class="form-text text-muted"><small>Enter your old password.</small></span>'
+        self.fields['old_password'].label = 'Old password'
+        self.fields['old_password'].help_text = ''
 
         self.fields['new_password1'].widget.attrs['class'] = 'form-control'
         self.fields['new_password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['new_password1'].label = ''
+        self.fields['new_password1'].label = 'New password'
         self.fields['new_password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['new_password2'].widget.attrs['class'] = 'form-control'
         self.fields['new_password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        self.fields['new_password2'].label = ''
+        self.fields['new_password2'].label = 'New password confirmation'
         self.fields['new_password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
 
     def clean(self):
